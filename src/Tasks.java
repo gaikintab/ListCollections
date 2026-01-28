efd77fbb39038d503936b90408c482a5da0d27b2
Gaikin <gaikintab@gmail.com>
2026-01-28 08:00:41 +0300

    Update4

--- src/Tasks.java
+++ src/Tasks.java
@@ -3,11 +3,11 @@ import java.util.List;
 
 public class Tasks {
 
-    protected List<String> listTasks = new ArrayList<>();
+    protected List<String> tasks = new ArrayList<>();
 
     public boolean addTask(String task){
-        if (!listTasks.contains(task)){
-            return listTasks.add(task);
+        if (!tasks.contains(task)){
+            return tasks.add(task);
         }
         return false;
     }
@@ -15,40 +15,40 @@ public class Tasks {
     public boolean deleteTaskByIndex(String[] deleteArr){
         List<String> delCol = new ArrayList<>();
         for (String digit : deleteArr){
-            int index = Integer.parseInt(digit) - 1;
-            if (index >= 0 && index < listTasks.size()){
-                delCol.add(listTasks.get(index));
+            int index = Integer.parseInt(digit.trim()) - 1;
+            if (index >= 0 && index < tasks.size()){
+                delCol.add(tasks.get(index));
             }
         }
         if (!delCol.isEmpty()){
-            return listTasks.removeAll(delCol);
+            return tasks.removeAll(delCol);
         }
         return false;
     }
 
     public boolean deleteTaskByName(String[] deleteArr){
         List<String> delCol = new ArrayList<>();
-        for (String task : listTasks){
+        for (String task : tasks){
             for (String word : deleteArr){
-                if (task.toLowerCase().contains(word.toLowerCase())){
+                if (task.toLowerCase().contains(word.toLowerCase().trim())){
                     delCol.add(task);
                     break;
                 }
             }
         }
         if (!delCol.isEmpty()){
-            return listTasks.removeAll(delCol);
+            return tasks.removeAll(delCol);
         }
         return false;
     }
 
     public String getTasks(){
         int counter = 0;
-        String taskList = "";
-        for(String task : listTasks){
-            counter++;
-            taskList += counter + ". " + task + "\n";
+        StringBuilder taskListBuilder = new StringBuilder();
+        for (String task : tasks){
+            taskListBuilder.append((counter++)).append(". ").append(task).append("\n");
         }
+        String taskList = taskListBuilder.toString();
         if (!taskList.isEmpty()){
             return taskList;
         }
